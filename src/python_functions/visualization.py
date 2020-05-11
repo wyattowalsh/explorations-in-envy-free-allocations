7ab664c86c7e979ceddc5eb9a09c143c3e8773d9
Wyatt Walsh <wyattowalsh@gmail.com>
2020-05-10 20:42:02 -0700

    adding files

--- src/python_functions/visualization.py
+++ src/python_functions/visualization.py
@@ -1,4 +1,5 @@
-# This file holds the visualization functions used throughout the course of Explorations in Envy-Free Allocations
+# This file holds the visualization functions used throughout the course
+# of Explorations in Envy-Free Allocations
 
 import imgkit
 import ipywidgets as widgets
@@ -10,52 +11,57 @@ import seaborn as sns
 from IPython.display import HTML, Image, display
 from mpl_toolkits.mplot3d import Axes3D
 
-solve_result_mapping = {
-    'optimal solution found': 99,
-    'optimal solution indicated, but error likely': 199,
-    'constraints cannot be satisfied': 299,
-    'objective can be improved without limit': 399,
-    'stopped by a limit (such as on iterations)': 499,
-    'stopped due to solver error': 599
-}
-columns = [
-    'Solver Status', 'Allocation p-Envy-Free Value', 'Solver Elapsed Time',
-    'Solver Total Elapsed Time', 'Number of People', 'Number of Items',
-    'Associated File Number'
-]
-styles = [
-    dict(selector="caption",
-         props=[("text-align", "center"), ("font-size", "120%"),
-                ("color", 'black')]), {
-                    'props': [("font-family", "Calibri")]
-                }, {
-                    'selector': 'th',
-                    'props': [('text-align', 'center')]
-                }
-]
+
 cm = sns.light_palette("#003262", as_cmap=True)
-pal =  sns.light_palette("#003262")
+pal = sns.light_palette("#003262")
+
 
 def tabular_statistics(rel_path_to_results,
                        grouped_title,
                        overall_title,
-                       columns=columns,
                        name=None,
                        save=False,
                        cash=False):
+
+    solve_result_mapping = {
+        'optimal solution found': 99,
+        'optimal solution indicated, but error likely': 199,
+        'constraints cannot be satisfied': 299,
+        'objective can be improved without limit': 399,
+        'stopped by a limit (such as on iterations)': 499,
+        'stopped due to solver error': 599
+    }
+    columns = [
+        'Solver Status', 'Allocation p-Envy-Free Value', 'Solver Elapsed Time',
+        'Solver Total Elapsed Time', 'Number of People', 'Number of Items',
+        'Associated File Number'
+    ]
+    styles = [
+        dict(selector="caption",
+             props=[("text-align", "center"), ("font-size", "120%"),
+                    ("color", 'black')]), {
+                        'props': [("font-family", "Calibri")]
+        }, {
+                        'selector': 'th',
+                        'props': [('text-align', 'center')]
+        }
+    ]
+
     results = pd.read_csv(rel_path_to_results, sep=',')
     results['solveResult'] = pd.cut(results['solveResult'],
                                     bins=[0] +
                                     list(solve_result_mapping.values()),
                                     labels=list(solve_result_mapping.keys()))
     if cash:
-        results.columns = columns.append('Subsidy Amount')
+        columns.insert(-1, 'Subsidy Amount')
+        results.columns = columns
         results = results.style.background_gradient(
             cmap=cm,
             subset=[
                 'Allocation p-Envy-Free Value', 'Solver Elapsed Time',
                 'Number of People', 'Number of Items', 'Subsidy Amount'
             ])
+        results.format({'Subsidy Amount': '${0:}'})
     else:
         results.columns = columns
         results = results.drop('Solver Total Elapsed Time', axis=1)
@@ -81,6 +87,7 @@ def tabular_statistics(rel_path_to_results,
                            './visualizations/{}_results.png'.format(name))
     return results.data, results, grouped
 
+
 def create_bar_plot(data, type_of, name=None, save=False):
     data = data.loc[data['Number of Items'] %
                     2 == 0].loc[data['Number of People'] % 2 == 0]
@@ -100,14 +107,18 @@ def create_bar_plot(data, type_of, name=None, save=False):
                    fontsize=28)
     g.set_yticklabels(g.ax.get_yticks(), size=18)
     g.set_xticklabels(size=18)
-    g.ax.set_yscale('log')
+    if name != "aef1":
+        g.ax.set_yscale('log')
+
+    else:
+        g.ax.set_ylim(0, 1)
     if save:
         if type_of == "Solver Elapsed Time":
             to_save = "time"
         else:
             to_save = 'envy'
         plt.savefig("./visualizations/barcharts/{}_{}".format(name, to_save),
-                    dpi=900,
+                    dpi=300,
                     bbox_inches='tight')
     return g
 
@@ -120,6 +131,7 @@ def create_bar_plot(data, type_of, name=None, save=False):
 #                          cmap=plt.cm.jet,
 #                          linewidth=0.01)
 #     return ax
+
 
 def plot_scatter(data, name, save=False):
     people = data.groupby("Number of People").mean().reset_index()
@@ -164,9 +176,10 @@ def plot_scatter(data, name, save=False):
 
     if save:
         f.savefig("./visualizations/scatterplots/{}".format(name),
-                  dpi=900,
+                  dpi=300,
                   bbox_inches='tight',
                   bbox_extra_artists=[st])
+
 
 def heatmap(data, type_of, name=None, save=False):
     data = data[['Number of People', 'Number of Items',
@@ -183,20 +196,45 @@ def heatmap(data, type_of, name=None, save=False):
             to_save = 'envy'
         p.get_figure().savefig("./visualizations/heatmaps/{}_{}".format(
             name, to_save),
-                               dpi=900,
-                               bbox_inches='tight')
+            dpi=300,
+            bbox_inches='tight')
     return p
 
+
 def parallel_categories(data, type_of, name=None, save=False):
-    fig = px.parallel_categories(
-        data,
-        dimensions=['Number of People', "Number of Items"],
-        color=type_of,
-        color_continuous_scale=px.colors.sequential.Blues,
-        title="Parallel Categories Plot for {}".format(type_of),
-        width=1000,
-        height=500)
-    fig.update_layout(title_x=0.5, hoverlabel=None)
+    if name == 'aefs':
+        fig = px.parallel_categories(
+            data,
+            dimensions=['Number of People',
+                        "Number of Items", "Subsidy Amount"],
+            color=type_of,
+            color_continuous_scale=px.colors.sequential.Blues,
+            title="Parallel Categories Diagram for {}".format(type_of),
+            width=1200,
+            height=500)
+        fig.update_layout(title_x=0.5, hoverlabel=None)
+    else:
+        if name == 'aef1':
+            fig = px.parallel_categories(
+                data,
+                dimensions=['Number of People', "Number of Items"],
+                color=type_of,
+                color_continuous_scale=px.colors.sequential.Blues,
+                title="Parallel Categories Diagram for {}".format(type_of),
+                width=1000,
+                height=500,
+                range_color=[0, 1])
+        else:
+
+            fig = px.parallel_categories(
+                data,
+                dimensions=['Number of People', "Number of Items"],
+                color=type_of,
+                color_continuous_scale=px.colors.sequential.Blues,
+                title="Parallel Categories Diagram for {}".format(type_of),
+                width=1000,
+                height=500)
+        fig.update_layout(title_x=0.5, hoverlabel=None)
     if save:
         if type_of == "Solver Elapsed Time":
             to_save = "time"
@@ -204,5 +242,35 @@ def parallel_categories(data, type_of, name=None, save=False):
             to_save = 'envy'
         fig.write_image("./visualizations/parallelcategories/{}_{}.png".format(
             name, to_save),
-                        format='png')
-    return fig
+            format='png')
+        return fig
+
+def scatter_4d(data, type_of, name=None, save=False):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    cash = [50, 100, 150, 200, 250, 300, 400, 600, 800]
+    for i, m in enumerate(['o', '^', 'v', '<', ">", '1', '+', 'x', 's']):
+        df_cash = data.loc[data['Subsidy Amount'] == cash[i]]
+        ax.scatter(df_cash['Number of Items'],
+                   df_cash['Number of People'],
+                   df_cash[type_of],
+                   marker=m,
+                   label='${}'.format(cash[i]),
+                   alpha=0.5)
+
+    ax.set_xlabel('Number of Items')
+    ax.set_ylabel('Number of People')
+    ax.set_zlabel(type_of)
+    ax.set_title("3D Scatter Plot of {} for Different Subsidy Values".format(
+        type_of), fontsize=10)
+    plt.legend(bbox_to_anchor=(0, 0.8), loc="upper left", prop={'size': 6})
+    if save:
+        if type_of == "Solver Elapsed Time":
+            to_save = "time"
+        else:
+            to_save = 'envy'
+        ax.get_figure().savefig("./visualizations/4dscatters/{}_{}".format(
+            name, to_save),
+            dpi=300,
+            bbox_inches='tight')
+    return ax
